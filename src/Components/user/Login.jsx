05441f5266419c6c44cbcb6cd9634dbd24e9eb12
hargunSingh13 <125636170+hargunSingh13@users.noreply.github.com>
2025-05-28 20:18:12 +0530

    Update Login.jsx

--- src/Components/user/Login.jsx
+++ src/Components/user/Login.jsx
@@ -55,7 +55,7 @@ const Login = () => {
     <path
       d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
   </svg>
-  <input value = {email} onChange={(e)=>setEmail(e.target.value)} type="email" className="grow" placeholder="Email" />
+  <input value = {email} onChange={(e)=>setEmail(e.target.value)} type="email" className="grow" placeholder="Email" required />
 </label>
 
 <label className="input input-bordered flex items-center gap-2 mt-5">
@@ -69,7 +69,7 @@ const Login = () => {
       d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
       clipRule="evenodd" />
   </svg>
-  <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" className="grow" placeholder="Password" />
+  <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" className="grow" placeholder="Password" required />
 </label>
 <center>
 <button type='submit' className='btn btn-primary mt-5 w-96'>Submit</button>
