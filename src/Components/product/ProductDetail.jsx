a33125c309cf627e083301849d70f75065456e5d
hargunSingh13 <125636170+hargunSingh13@users.noreply.github.com>
2025-05-28 21:04:42 +0530

    Update ProductDetail.jsx

--- src/Components/product/ProductDetail.jsx
+++ src/Components/product/ProductDetail.jsx
@@ -10,7 +10,7 @@ import Footer from '../Footer';
 const ProductDetail = () => {
   const [product, setProduct] = useState("");
   const {id} = useParams();
-  const url = 
+  const url = "https://regional-food-ecommerce-platform-backend.onrender.com/api"
   const {addToCart} = useContext(AppContext);
 
   useEffect(()=>{
