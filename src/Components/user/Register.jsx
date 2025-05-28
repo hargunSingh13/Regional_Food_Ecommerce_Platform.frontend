df835c914b20352a5ca6ea1a93cf42f64d712abb
hargunSingh13 <125636170+hargunSingh13@users.noreply.github.com>
2025-05-28 20:16:06 +0530

    Update Register.jsx

--- src/Components/user/Register.jsx
+++ src/Components/user/Register.jsx
@@ -52,7 +52,7 @@ const Register = () => {
     <path
       d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
   </svg>
-  <input value = {name} onChange={(e)=>setName(e.target.value)}  type="text" className="grow" placeholder="Username" />
+  <input value = {name} onChange={(e)=>setName(e.target.value)}  type="text" className="grow" placeholder="Username" required />
 </label>
 
 <label className="input input-bordered flex items-center gap-2 mt-5">
@@ -66,7 +66,7 @@ const Register = () => {
     <path
       d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
   </svg>
-  <input value = {email} onChange={(e)=>setEmail(e.target.value)}  type="email" className="grow" placeholder="Email" />
+  <input value = {email} onChange={(e)=>setEmail(e.target.value)}  type="email" className="grow" placeholder="Email" required />
 </label>
 
 <label className="input input-bordered flex items-center gap-2 mt-5">
@@ -80,7 +80,7 @@ const Register = () => {
       d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
       clipRule="evenodd" />
   </svg>
-  <input value = {password} onChange={(e)=>setPassword(e.target.value)}  type="password" className="grow" placeholder="Password" />
+  <input value = {password} onChange={(e)=>setPassword(e.target.value)}  type="password" className="grow" placeholder="Password" required />
 </label>
 <center>
 <button type='submit' className='btn btn-primary mt-5 w-96'>Submit</button>
